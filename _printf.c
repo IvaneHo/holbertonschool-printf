2ac7ab25dd62ff43c703c19f38ae6d789af05acc
IvaneHo <10608@holbertonstudents.com>
2025-03-25 09:35:39 +0100

    changement pour betty style

--- _printf.c
+++ _printf.c
@@ -1,22 +1,40 @@
 #include "main.h"
-
 /**
- * _printf - Fonction qui reproduit une version simplifiée de printf
- * @format: Chaîne de caractères à imprimer (peut contenir des formats)
+ * print_format - Gère l'affichage des formats spéciaux (%c, %s, %%)
+ * @format: Format à vérifier
+ * @args: Liste des arguments
  *
- * Return: Nombre de caractères affichés (comme printf)
+ * Return: Nombre de caractères imprimés
  */
-int _printf(const char *format, ...)
+int print_format(char format, va_list args)
 {
-	va_list args;         /* Pour récupérer les arguments variables */
-	int i = 0;            /* Index pour parcourir la chaîne format */
-	int count = 0;        /* Compteur de caractères imprimés */
-	char *str;            /* Pour stocker les chaînes à imprimer */
+	int count = 0;
+	char *str;
 
-	if (format == NULL)   /* Vérifie si la chaîne est valide */
-		return (-1);
-
-	va_start(args, format); /* Initialise les arguments variables */
-
-	
+	if (format == 'c')
+	{	char c = va_arg(args, int);
+		write(1, &c, 1);
+		count++;
+	}
+	else if (format == 's')
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+			str = "(null)";
+		while (*str)
+		{	write(1, str, 1);
+			str++;
+			count++;
+		}
+	}
+	else if (format == '%')
+	{	write(1, "%", 1);
+		count++;
+	}
+	else
+	{	write(1, "%", 1);
+		write(1, &format, 1);
+		count += 2;
+	}
+	return (count);
 }
