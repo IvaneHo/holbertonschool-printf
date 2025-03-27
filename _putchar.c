8d328c93440b2476b83bfa7b752fb0448fefea6f
IvaneHo <10608@holbertonstudents.com>
2025-03-27 19:54:28 +0100

    Modification commentaires

--- _putchar.c
+++ _putchar.c
@@ -4,6 +4,9 @@
  * _putchar - Écrit un caractère sur stdout
  * @c: caractère à afficher
  *
+ * Utilise la fonction write() pour afficher un seul caractère
+ * sur la sortie standard (file descriptor 1).
+ *
  * Return: 1 si succès, -1 si erreur
  */
 int _putchar(char c)
